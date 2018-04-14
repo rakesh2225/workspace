c440cb257ab4e9bdaf98a26f4c0a0dcfeffd6a6c
Rakesh <svr2225@gmail.com>
2018-04-14 15:45:35 -0400

    Adding BigNumber Calculator assignment3

---
+++ MS/AdvancedProgramming/BigNumberCalculator/src/bignumbercalculator/FXMLDocumentController.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bignumbercalculator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author minaly
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label lblNum1;
+    private Label lblNum2;
+    private Label lblResult;
+    private TextField txtNum1;
+    private TextField txtNum2;
+    private TextField txtResult;
+    private Button btnComp;
+    private Button btnQuit;
+    private Button btnAdd;
+    private Button btnSub;
+    private Button btnMul;
+    private Button btnGrt;
+    private Button btnLess;
+    private Button btnEqls;
+    private Label lblOptr;
+    
+    @FXML
+    private void handleComputeButtonAction(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleQuitButtonAction(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleAddButtonAction(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleSubButtonAction(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleMulButtonAction(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleGreaterButtonAction(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleLessButtonAction(ActionEvent event) {
+       
+    }
+    
+    @FXML
+    private void handleEqualButtonAction(ActionEvent event) {
+       
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
